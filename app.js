315208635040e3a209703b6c89dce388d824ec16
Rajput-Developer <ahsanrehman728@gmail.com>
2024-09-28 12:07:34 -0700

    changing the port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import { GetAllBlogs } from './controllers/blog.controller.js';
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 // DBConnection
 async function DBConnection() {
